7e4162c9d310e2483fed2c8260790e772a116569
Christopher Navarro <navarro.christopher16@yahoo.com>
2023-10-24 11:33:01 -0700

    upload

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -1,13 +1,31 @@
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import styles from "./NavigationBar.module.css";
 
-function NavigationBar ()
-{
+function NavigationBar() {
+  const routes = [
+    { to: "/", text: "Home" },
+    { to: "/spacecrafts", text: "Spacecrafts" },
+    { to: "/planets", text: "Planets" },
+  ];
+
   return (
-    {
-      // todo
-    }
+    <nav className={styles["navigation"]}>
+      {
+      routes.map((route, index) =>
+        <NavLink
+          key={index}
+          className={({ isActive, isPending }) =>
+            `${styles["navigation__item"]} ${
+              isActive ? styles["navigation__item--active"] : ""
+            }`
+          }
+          to={route.to}
+        >
+          {route.text}
+        </NavLink>
+      )}
+    </nav>
   );
 }
 
